2db35c8fbd080038eaf9b9ba4305ce31103de17b
Latrese <latrese.flowers1979@gmail.com>
2019-06-29 10:57:56 -0700

    revising

--- assets/app.js
+++ assets/app.js
@@ -6,23 +6,30 @@
       authDomain: "my-train-project-88054.firebaseapp.com",
       databaseURL: "https://my-train-project-88054.firebaseio.com",
       projectId: "my-train-project-88054",
-      storageBucket: "",
+      storageBucket: "my-train-project-88054.appspot.com",
       messagingSenderId: "112256562833",
       appId: "1:112256562833:web:7eeb433a50a6e796"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-    
-    var db = firebase.database();
-       var trainRef = db.ref("/train");
-    
-    $(".submit-button").on("click", function(event){
-      event.preventDefault();
-      console.log("i was clicked")
-      var trainName = $("trainName").val()
 
-      trainRef.push({
-        train : train.trainName
-      })
-      }) 
+    var db = firebase.database();
+    
+$('.submit-button').on('click', function(event) {
+  event.preventDefault();
+  var trainName = $('#trainName').val();
+  var destination = $('#destination').val();
+  var trainTime = $('#trainTime').val();
+  var frequency = $('#frequency').val();
+
+  var newTrain = {
+    trainName : trainName,
+    destination : destination,
+    trainTime : trainTime,
+    frequency : frequency
+  }
+ db.ref().push(newTrain);
+  console.log(trainName)
+
+})
 });
