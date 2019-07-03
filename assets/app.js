2169a9a4e8abe5857f85bc1e65a48954be2c7ed7
Latrese <latrese.flowers1979@gmail.com>
2019-07-02 23:51:21 -0700

    morerevisions

--- assets/app.js
+++ assets/app.js
@@ -14,22 +14,43 @@
     firebase.initializeApp(firebaseConfig);
 
     var db = firebase.database();
+  
+    var trainName = "";
+    var destination = "";
+    var trainTime = "";
+    var frequency = "";
     
 $('.submit-button').on('click', function(event) {
   event.preventDefault();
-  var trainName = $('#trainName').val();
-  var destination = $('#destination').val();
-  var trainTime = $('#trainTime').val();
-  var frequency = $('#frequency').val();
 
-  var newTrain = {
+  trainName = $('#trainName').val().trim();
+  destination = $('#destination').val().trim();
+  trainTime = $('#trainTime').val().trim();
+  frequency = $('#frequency').val().trim();
+
+  db.ref().push({
     trainName : trainName,
     destination : destination,
     trainTime : trainTime,
-    frequency : frequency
-  }
- db.ref().push(newTrain);
-  console.log(trainName)
+    frequency : frequency,  
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  })
+
+  db.ref().on("child_added", function(childSnapshot){
+    var newTrain = snapshot.val();
+    console.log(childSnapshot.val().trainName);
+    console.log(childSnapshot.val().destination);
+    console.log(childSnapshot.val().trainTime);
+    console.log(childSnapshot.val().frequency);
+
+  })
+  
+  db.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
+    $('#trainName-display').text(snapshot.val().trainName);
+    $('#destination-display').text(snapshot.val().destination);
+    $('#trainTime-display').text(snapshot.val().trainTime);
+    $('#frequency-display').text(snapshot.val().frequency);
+  })
 
 })
 });
